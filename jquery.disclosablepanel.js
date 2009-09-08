125ce0551d1b80476cb9c58c697c9760df4ec8de
Vagmi Mudumbai <vagmi@deepthought.(none)>
2009-09-08 16:00:57 +0530

    respect properties such as width and title

--- jquery.disclosablepanel.js
+++ jquery.disclosablepanel.js
@@ -1,18 +1,25 @@
 (function($){
  $.fn.disclosable = function(options){
-  var self=this;
-  this.original_title=this.attr('title');
+  this.each(function(){
+  var self=$(this);
+  self.original_title=self.attr('title');
   panel_classes="ui-disclosable ui-dialog ui-widget ui-widget-content ui-corner-all";
   panel_title_bar_classes="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix";
   panel_title_classes="ui-dialog-title";
   panel_toggle_classes="ui-dialog-titlebar-close ui-corner-all";
   panel_toggle_link_classes="ui-icon ui-icon-circle-triangle-n";
-  this.hide();
-  panel=$('<div></div>').addClass(panel_classes).insertBefore(this).css("margin","0.5em 0");
-  panel_title_bar=$('<div></div>').addClass(panel_title_bar_classes).appendTo(panel);
-  panel_title=$('<span></span>').addClass(panel_title_classes).appendTo(panel_title_bar).text(this.original_title);
-  toggle_link=$('<a href="#"/>').addClass(panel_toggle_classes).appendTo(panel_title_bar)
-  toggle_link_span=$('<span/>').addClass(panel_toggle_link_classes).text("close").appendTo(toggle_link);
+  self.hide();
+  var panel=$('<div></div>').addClass(panel_classes).insertBefore(self).css("margin","0.5em 0");
+  if(options.hasOwnProperty('width')){
+    panel.css('width',options.width);
+  }
+  var panel_title_bar=$('<div></div>').addClass(panel_title_bar_classes).appendTo(panel);
+  var panel_title=$('<span></span>').addClass(panel_title_classes).appendTo(panel_title_bar).text(self.original_title);
+  if(options.hasOwnProperty('title')){
+    panel_title.text(options.title);
+  }
+  var toggle_link=$('<a href="#"/>').addClass(panel_toggle_classes).appendTo(panel_title_bar)
+  var toggle_link_span=$('<span/>').addClass(panel_toggle_link_classes).text("close").appendTo(toggle_link);
   toggle_link.click(function(){
     if(self.css("display")=="block"){
       self.hide('normal');
@@ -27,7 +34,8 @@
       }
       return false;
     });
-  this.appendTo(panel).show();
-  console.log($(this).attr("title"));
- }
- })(jQuery);
+  self.appendTo(panel).show();
+  console.log($(self).attr("title"));
+ });
+  return this;
+ }})(jQuery);
